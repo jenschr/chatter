34cdf28dda76ab50aecec0b929d0db12d583bb6f
jenschr <jenschr@gmail.com>
2022-10-25 11:49:00 +0200

    test

--- src/App.js
+++ src/App.js
@@ -188,7 +188,7 @@ function App() {
       </div>
 
       <div className="w3-container w3-black w3-center w3-opacity w3-padding-64">
-          <h1 className="w3-margin w3-xlarge">Quote of the day: live life</h1>
+          <h1 className="w3-margin w3-xlarge">Quote of the day: live life!</h1>
       </div>
 
       {/* Footer */}
